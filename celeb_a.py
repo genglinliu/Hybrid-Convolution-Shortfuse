0674491d089290a4889433417df24d4e072349e0
genglinliu <44108482+genglinliu@users.noreply.github.com>
2021-03-24 16:22:16 -0400

    added back old code

--- celeb_a.py
+++ celeb_a.py
@@ -1,7 +1,6 @@
 from __future__ import print_function, division
 
 import numpy as np
-import pandas as pd
 import matplotlib.pyplot as plt
 
 import torch
@@ -12,25 +11,18 @@ import torchvision
 from torchvision import models, transforms, datasets
 
 import torch.optim as optim
+from torch.optim import lr_scheduler
+
 from torch.utils.data import DataLoader
+from torch.utils.data import sampler
 
 import PIL.Image as Image
 from tqdm import tqdm
-import os
+
 import time
-
-from sklearn.metrics import f1_score
-
-from model.vgg_16 import *
-from model.hybrid_CNN import *
-
-
 
 # get dataset
 def load_data(batch_size):
-    """
-    return the train/val/test dataloader
-    """
     
     transform = transforms.Compose([
         transforms.Resize((224, 224)),
@@ -68,86 +60,54 @@ def load_data(batch_size):
     return train_loader, val_loader, test_loader
 
 
-def initialize_model(model, learning_rate, num_classes, device):
-    """
-    initialize the model (pretrained vgg16_bn)
-    define loss function and optimizer and move data to gpu if available
-    
-    return:
-        model, loss function(criterion), optimizer
-    """
-    
+def initialize_model(learning_rate, num_classes, device):
+    model = models.vgg16_bn(pretrained=True)
     num_ftrs = model.classifier[6].in_features
     model.classifier[6] = nn.Linear(num_ftrs, num_classes)
     model = model.to(device)
     # Define loss function and optimizer
     criterion = nn.CrossEntropyLoss()
-    # criterion = nn.BCELoss()
-
     optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
     return model, criterion, optimizer
 
-def make_plots(step_hist, loss_hist, epoch=0):
-    plt.plot(step_hist, loss_hist)
-    plt.xlabel('train_iterations')
-    plt.ylabel('Loss')
-    plt.title('epoch'+str(epoch+1))
-    plt.savefig('plot1')
-    plt.clf()
-
 
 def train(train_loader, model, criterion, optimizer, num_epochs, device):
-    """
-    Move data to GPU memory and train for specified number of epochs
-    Also plot the loss function and save it in `Figures/`
-    Trained model is saved as `cnn.ckpt`
-    """
     for epoch in range(num_epochs): # repeat the entire training `num_epochs` times
         # for each training sample
         loss_hist = []
         step_hist = []
         for i, (images, labels) in tqdm(enumerate(train_loader)):
-            label = labels[:, 2]   # attractiveness label
-            cov_attr = labels[:, 20]    # gender (male/female)   
-            cov_attr = (cov_attr + 1) // 2  # map from {-1, 1} to {0, 1}
-            
             # move to gpu if available
+            labels = labels[:, 2] # attractiveness label
             images = images.to(device)
-            label = label.to(device)
-            
+            labels = labels.to(device)
             # forward pass
-            # outputs = model(images, cov_attr)    # model takes covariate here
             outputs = model(images)
-            loss = criterion(outputs, label) 
+            loss = criterion(outputs, labels) # still a tensor so we need to use .item() when printing
             
             # backward
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
-            
-            # TODO: print AND log
+            # print
             if (i+1) % 100 == 0:
                 print('Epoch: [{}/{}], Step[{}/{}], Loss:{:.4f}' \
-                        .format(epoch+1, num_epochs, i+1, len(train_loader), loss.item()))
-                with open('output.txt', 'a') as f:
-                    print('Epoch: [{}/{}], Step[{}/{}], Loss:{:.4f}' \
-                        .format(epoch+1, num_epochs, i+1, len(train_loader), loss.item()), file=f)
+                    .format(epoch+1, num_epochs, i+1, len(train_loader), loss.item()))
                 loss_hist.append(loss.item())
                 step_hist.append(i+1)
         
-        make_plots(step_hist, loss_hist, epoch)
+        plt.plot(step_hist, loss_hist)
+        plt.xlabel('train_iterations')
+        plt.ylabel('Loss')
+        plt.title('epoch'+str(epoch+1))
+        plt.savefig('epoch_1')
+        plt.clf()
         
-    torch.save(model.state_dict(), 'cnn_with_cov.ckpt')
+    torch.save(model.state_dict(), 'cnn.ckpt')
 
 
 def evaluate(val_loader, model, device):
-    """
-    Run the validation set on the trained model
-    """
-    model_path = "model/cnn.ckpt"
-    state_dict = torch.load(model_path)
-    model.load_state_dict(state_dict)
-    model.eval() 
+    model.eval() # BatchNorm uses moving mean/variance instead of mini-batch mean/variance
     with torch.no_grad():
         # initialize the stats
         correct = 0
@@ -155,50 +115,39 @@ def evaluate(val_loader, model, device):
         # pass through testing data once
         for images, labels in val_loader:
 
-            label = labels[:, 2]
-            cov_attr = labels[:, 20]    # gender (male/female)   
-            cov_attr = (cov_attr + 1) // 2  # map from {-1, 1} to {0, 1}
-            
+            labels = labels[:, 2]
             # again move to device first
             images = images.to(device)
-            label = label.to(device)
-            
-            # forward
-            # outputs = model(images, cov_attr)
+            labels = labels.to(device)
+            # forward once
             outputs = model(images)
+            # instead of calculating loss we will get predictions
+            # it's essetially outputs just reformatting imo
             _, predicted = torch.max(outputs.data, 1)
-            
             # accumulate stats
-            total += label.size(0) # yeah again, number of elements in the tensor
-            correct += (label == predicted).sum().item()
-        print('Validation accuracy: {}%'.format(100 * correct / total))
-        with open('output.txt', 'a') as f:
-            print('Validation accuracy: {}%'.format(100 * correct / total), file=f)
+            total += labels.size(0) # yeah again, number of elements in the tensor
+            correct += (labels == predicted).sum().item()
+
+        # print
+        print('Test accuracy on 10000 test images: {}%' \
+                .format(100 * correct / total))
     
     
 def main():
-    # hyper parameters    
+    # hyper parameters
     num_epochs = 1
     num_classes = 2
-    batch_size = 1 # WE WANT IMAGES TO PASS HYBRID CONV LAYER ONE BY ONE
+    batch_size = 16
     learning_rate = 0.001
-    # model_name = MyVGG16()
-    # model_name = vgg16(pretrained=True)
-    model_name = models.vgg16_bn(pretrained=True)
+    model_name = "vgg-16"
     
     device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-    
-    print("Loading data...")
     train_loader, val_loader, test_loader = load_data(batch_size)
     
-    print("Initializing model...")
-    model, criterion, optimizer = initialize_model(model_name, learning_rate, num_classes, device)
-   
-    print("Start training... \n")
+    model, criterion, optimizer = initialize_model(learning_rate, num_classes, device)
     train(train_loader, model, criterion, optimizer, num_epochs, device)
+    evaluate(val_loader, model, device)
     
-    print("Start evaluating... \n")
-    evaluate(val_loader, model, device)    
 
 if __name__ == "__main__":
     main()
